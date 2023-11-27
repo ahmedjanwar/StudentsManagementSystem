62857c19b16d23674d5136484349c8b9c6346bb3
ahmedjanwar <ajemal840@gmail.com>
2023-11-27 18:01:04 +0200

    testing

--- server/StudentsManagementSystem/src/main/java/com/school/restfulAPI/SecurityConfig.java
+++ server/StudentsManagementSystem/src/main/java/com/school/restfulAPI/SecurityConfig.java
@@ -6,14 +6,34 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
     protected void configure(HttpSecurity http) throws Exception {
-        // Configure your security rules here
+        http
+                .authorizeRequests(requests -> requests
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/dashboard")
+                        .permitAll())
+                .logout(logout -> logout
+                        .permitAll());
     }
 }
-
 
